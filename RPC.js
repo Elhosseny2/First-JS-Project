789971f2ee5b5669ad40830ad6c1d4b96d2b14b2
Elhosseny2 <moelhosiny8@gmail.com>
2024-08-26 01:25:39 +0300

    TOP fucked me up !!!

---
+++ RPC.js
@@ -0,0 +1,37 @@
+function getComputerChoice() {
+  let arr = ["rock", "paper", "scissors"];
+  let PC = arr[Math.floor(Math.random() * arr.length)];
+  return PC;
+}
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+  let ans = prompt(" Choose a Symbol ");
+  while (true) {
+    if (ans == "rock" || ans == "paper" || ans == "scissors") {
+      break;
+    } else {
+      ans = prompt("invalid symbol, try again.");
+    }
+  }
+  return ans;
+}
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice,computerChoice) {
+  if (humanChoice == computerChoice) {
+    alert("You and PC are tied!");
+  } else if (humanChoice == "rock" && computerChoice == "scissors") {
+    alert("You win! congratulations!");
+  } else if (humanChoice == "paper" && computerChoice == "rock") {
+    alert("You win congratulations");
+  } else if (humanChoice == "scissors" && computerChoice == "paper") {
+    alert("You win congratulations");
+  } else {
+    alert("You loss, PC wins!");
+  }
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
