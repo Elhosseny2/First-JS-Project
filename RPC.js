b0e516c05dc5d3162f6e549422d39e747b3943bb
Elhosseny2 <moelhosiny8@gmail.com>
2024-09-26 10:52:56 +0300

    End the project, done with all changes
    I completely forgot to commit within working on
    the project (i was overwhelmed).

--- RPC.js
+++ RPC.js
@@ -1,63 +1,125 @@
 function getComputerChoice() {
   let arr = ["rock", "paper", "scissors"];
   let PC = arr[Math.floor(Math.random() * arr.length)];
+  console.log (PC)
   return PC;
 }
-function getHumanChoice() {
-  let ans = prompt("Choose a Symbol: rock, paper, or scissors").toLowerCase();
-  while (true) {
-    if (ans == "rock" || ans == "paper" || ans == "scissors") {
-      break;
-    } else {
-      ans = prompt(
-        "Invalid symbol, try again: rock, paper, or scissors"
-      ).toLowerCase();
-    }
-  }
-  return ans;
-}
+
 let humanScore = 0;
 let computerScore = 0;
+let draw = 0;
 
 function playRound(humanChoice, computerChoice) {
   if (humanChoice == computerChoice) {
-    alert("It's a tie!");
+    //user is not demanded to write an answer any more. 
+    //so commented the alert massages to, to make it unique with winning or losing cases.
+    // alert("It's a tie!");
+    draw++;
   } else if (
     (humanChoice == "rock" && computerChoice == "scissors") ||
     (humanChoice == "scissors" && computerChoice == "paper") ||
     (humanChoice == "paper" && computerChoice == "rock")
   ) {
-    alert("You win! Congratulations!");
+    // alert("You win! Congratulations!");
     humanScore++;
   } else {
-    alert("You lose, PC wins!");
+    // alert("You lose, PC wins!");
     computerScore++;
   }
 }
 
-function playGame () {
-  function playRound(humanChoice, computerChoice) {
-    if (humanChoice == computerChoice) {
-      alert("It's a tie!");
-    } else if (
-      (humanChoice == "rock" && computerChoice == "scissors") ||
-      (humanChoice == "scissors" && computerChoice == "paper") ||
-      (humanChoice == "paper" && computerChoice == "rock")
-    ) {
-      alert("You win! Congratulations!");
-      humanScore++;
-    } else {
-      alert("You lose, PC wins!");
-      computerScore++;
-    }
+
+let humanSelection;
+let computerSelection = getComputerChoice();
+
+let rockBtn = document.querySelector("#rock");
+let paperBtn = document.querySelector("#paper");
+let scissorsBtn = document.querySelector("#scissors");
+
+function disableButtons() {
+    // Disable all buttons, not a mandatory task, but essential for the logic.
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
+    results.textContent = (`The Final Result is :player: ${humanScore}, Computer: ${computerScore}, Draw: ${draw} `)
   }
-  let humanScore = 0;
-  let computerScore = 0;
-  for (let i =0; i <5; i++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection , computerSelection)
-  }``
-  console.log(`Scores:\nHuman: ${humanScore}\nPC: ${computerScore}`);
-}
-playGame();
+
+rockBtn.addEventListener("click", () => {
+    let humanSelection = "rock";
+    playRound(humanSelection, getComputerChoice());
+    console.log(humanScore, computerScore, draw);
+    results.textContent= `The Result is : player: ${humanScore}, Computer: ${computerScore}, Draw: ${draw} `
+    if (humanScore === 5) {
+       alert ("Congratulations, You beat the Computer!");
+       disableButtons();
+    }
+    else if ( computerScore === 5 ) {
+        alert ("Game over, good luck next time");
+        disableButtons();
+    }
+
+});
+paperBtn.addEventListener("click", () => {
+  let humanSelection = "paper";
+  playRound(humanSelection, getComputerChoice());
+  console.log(humanScore, computerScore, draw);
+  results.textContent= `The Result is : player: ${humanScore}, Computer: ${computerScore}, Draw: ${draw} `
+  if (humanScore === 5) {
+    alert ("Congratulations, You beat the Computer!")
+    disableButtons();
+ }
+ else if ( computerScore === 5 ) {
+     alert ("Game over, good luck next time")
+     disableButtons();
+ }
+});
+
+scissorsBtn.addEventListener("click", () => {
+  let humanSelection = "scissors";
+  playRound(humanSelection, getComputerChoice());
+  console.log(humanScore, computerScore, draw);
+  results.textContent= `The Result is : player: ${humanScore}, Computer: ${computerScore}, Draw: ${draw} `
+  if (humanScore === 5) {
+    alert ("Congratulations, You beat the Computer!")
+    disableButtons();
+ }
+ else if ( computerScore === 5 ) {
+     alert ("Game over, good luck next time")
+     disableButtons();
+ }
+});
+
+let results = document.querySelector('#results');
+
+//////////////////////////////
+
+//comment-delete the logic that plays the game 5 times, (left as a reminder).
+// function playGame() {
+//   function playRound(humanChoice, computerChoice) {
+//     if (humanChoice == computerChoice) {
+//       alert("It's a tie!");
+//       draw++;
+//     } else if (
+//       (humanChoice == "rock" && computerChoice == "scissors") ||
+//       (humanChoice == "scissors" && computerChoice == "paper") ||
+//       (humanChoice == "paper" && computerChoice == "rock")
+//     ) {
+//       alert("You win! Congratulations!");
+//       humanScore++;
+//     } else {
+//       alert("You lose, PC wins!");
+//       computerScore++;
+//     }
+//   }
+//   let humanScore = 0;
+//   let computerScore = 0;
+//   let draw = 0;
+//   for (let i = 0; i < 5; i++) {
+//     const humanSelection = getHumanChoice();
+//     const computerSelection = getComputerChoice();
+//     playRound(humanSelection, computerSelection);
+//   }
+//   console.log(`Scores:\nHuman: ${humanScore}\nPC: ${computerScore}\nDraw: ${draw}`);
+// }
+// playGame();
+``
